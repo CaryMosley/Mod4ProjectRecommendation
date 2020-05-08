83077fa8560b1ae3ed771d41d73f511b6c02dd78
bryandps <bryan.santos@gmail.com>
2020-05-08 18:15:06 +0800

    final version

--- README.md
+++ README.md
@@ -14,27 +14,56 @@ Our presentation deck is located here: https://docs.google.com/presentation/d/1k
 
 The main issue with recommendation systems based on other user reviews (collaborative-filtering) are cold start and sparsity. As seen in the image below, movies being recommended by traditional movies are skewed towards the popular ones. The goal is to tap into this long tail and be able to recommend quality but less popular movies.
 
-![models](https://github.com/CaryMosley/Mod4ProjectRecommendation/blob/bryan-santos/images/longtail.png)
+![models](https://github.com/CaryMosley/Mod4ProjectRecommendation/blob/CaryM/images/longtail.png)
+
+## Preprocessing and EDA
+
+### Data Sources
+
+We loaded in various datasets provided by movielens and then ran an API call to grab plot summaries for each of our films. Once we had this we grabbed the union of films where we had movieId and plot summaries as well as dropped the handful of duplicates from our dataset. This resulted in 9573 total films and 100293 total ratings.
+
+### EDA
+Below we can see the ratings count as well as the distrubtio of ratings. THe most common ratings values were 4.0 and 3.0, with perfect films being significantly more common than very poorly rated films.
+
+![models](https://github.com/CaryMosley/Mod4ProjectRecommendation/blob/CaryM/images/ratings.png)
+
+![models](https://github.com/CaryMosley/Mod4ProjectRecommendation/blob/CaryM/images/ratingsdist.png)
+
+We can see that most users rated only a handful of films with a few super users rating over a thousand. This is likely to introduce bias as our model is going to be very skewed towards the rating preference of these power users.
+
+![models](https://github.com/CaryMosley/Mod4ProjectRecommendation/blob/CaryM/images/userdist.png)
+
+When we examile genre, Film-Noir has the highest rating of all genres followed by War, Documentary and Crime. Comedy and Horror are the lowest rated.
+
+![models](https://github.com/CaryMosley/Mod4ProjectRecommendation/blob/CaryM/images/genres.png)
+
+In this particular sample of dataset provided from Movielens, we see a sudden resurgence in the number of available ratings beginning from the year 2015.
+
+![models](https://github.com/CaryMosley/Mod4ProjectRecommendation/blob/CaryM/images/yeardist.png)
+
+As we showed in our problem statement there is a long-tail in movie popularity.
+
+![models](https://github.com/CaryMosley/Mod4ProjectRecommendation/blob/CaryM/images/longtail.png)
+
+We grouped our films into ratings buckets of top, average, and low and then created word clouds to see if plot could be useful in classifying our films. Based on these word clouds they will not perform well as the most common words seem to be very similar. 
+
+![models](https://github.com/CaryMosley/Mod4ProjectRecommendation/blob/CaryM/images/wordcloudtop.png)
+
+![models](https://github.com/CaryMosley/Mod4ProjectRecommendation/blob/CaryM/images/wordcloudavg.png)
+
+![models](https://github.com/CaryMosley/Mod4ProjectRecommendation/blob/CaryM/images/wordcloudlow.png)
 
 ## Feature Engineering
 
 ### Plot
 
-* We added plot summaries of each movie to run content-based models based on Natural Language Processing (NLP) and as foundation for Hybrid Models
-* Bag of Words and TF-IDF were added
-
-### Genres
+* We added plot summaries of each movie to run content-based models based on Natural Language Processing (NLP) and as foundation for Hybrid Models. We then crated a bag of words and TF-IDF feature for content similarity.
 
 Genres were included to ensure they play a factor in making the recommendation because chances are, people will like movies in the similar genre
 
-![models](https://github.com/CaryMosley/Mod4ProjectRecommendation/blob/bryan-santos/images/genres.png)
-
-### Quality Index
-We bucketed ratings in order to get movie clusters based on quality
-
-![models](https://github.com/CaryMosley/Mod4ProjectRecommendation/blob/bryan-santos/images/rating.png)
 
 ### Popularity Index
+
 We added popularity index based on the count of reviews on each movie
 
 ## Models
@@ -46,32 +75,45 @@ The following models were built and compared against each other using different 
 * User-User Filtering
 * Memory-Based Methods (KNN and SVD)
 
+Based on RMSE and Hit-Rate we chose the SVD model as our best collaborative-filtering based model. RMSE evaluates how far our rating is from the predicted. Hit-rate is a method in which we predict a number of films and then check to see how many if any were present in the user's list. This metric can run into issues with sparsity as if were looking predicting 10 movies and the user has only rated 5 then clearly the max hit rate can be is .5. However this metric is still useful when comparing between models. Next we will combine our SVD Collaborative-Flitering model with our chosen content-based model to create a final hybrid model.
+
+We also have a function within our notebook that will take in a new user ratings and then output recommendations.
+
 ### Content-Based
 * Bag of Words
 * TF-IDF
 
+We build 4 different classification models to see if TF-IDF combining plot and genre would be enough to group movies into top, average, and low ratings bucket. We used Naive Bayes, Logistic, SGD, and a neural net classifier. The Naive Bayes, SGD and neural net simply classified everything into the majority class, while the logisitic model did make some specific rankings. Although the accuray of the logistic model was lowest, when we look at the confusion matrix we can see that it is creating groupings into these distinct classes. However, as we saw from the word clouds, plot summary does not seem to be able to classify movie rankings alone. This is in-line with what we would expect and we then moved on to create a content based recommendation system using cosine similarty of our TF-IDF feature.
+
 ### Hybrid Models
 * **Hybrid Model 1** - running the TF-IDF model (content-based) first to get initial recommendations then the rating of each one is predicted through our best-performing SVD model (collaborative-filtering) which is then sorted
-* **Hybrid Model 2** - calculate the aggregate of cosine-similarity of movies based on TF-IDF model (content-based) and our best-performing SVD model (collaborative-filtering)
+
+* **Hybrid Model 2** - calculate the aggregate of cosine-similarity of movies based on TF-IDF model (content-based) and our best-performing SVD model (collaborative-filtering). We weight the similarity metrics of content and collaborative-filtering based on how popular the input movie is. Since our goal is to reach the long tail, we weight our results more towards content the higher the popularity of the film is. 
+
 * **Hybrid Model 3**- use LightFM, a python package well-known for hybrid modeling
+
+Once we built these models we played around using some of our favorite movies to attempt to evaluate which one we believed was giving us the best results. Based on this we decided to implement the first hybrid model in a front-end UI.
 
 ## Conclusion and Future Steps
 
-Our final model is Hybrid Model 2 which provides a balanced recommendation between Content-Based and Collaborative-Filtering
+Our final model is our Hybrid Model 1 which gets content-based recommendations and then predicts and sorts the results using our colabortive-filtering SVD model.
 
 ### Next Steps
-* Use the entire movielens dataset (2M) and not just (100k) to be able to cover most movies in the recommendation
+
+* Use the entire movielens dataset (2M) and not just (100k) to be able to cover most movies in the recommendation. Having significantly more ratings will help us avoid being biased towards the preferences of a few power users. We will also have more breadth in the type of movies we cna recommend 
+
 * Add more features and create a better ratings predictor for our hybrid model
-* Compile a user profile that is not solely based on ratings they have made but information about the users themselve
+
+* Compile a user profile that is not solely based on ratings they have made but information about the users themselves.
 
 ## Live Front-End
-We have a live web-based front-end created through Flask. The engine under the hood is Hybrid Model 2.
+We have a live web-based front-end created through Flask. The engine under the hood is Hybrid Model 1.
 
 **Search Page**
 
-![models](https://github.com/CaryMosley/Mod4ProjectRecommendation/blob/bryan-santos/images/live1.png)
+![models](https://github.com/CaryMosley/Mod4ProjectRecommendation/blob/CaryM/images/live1.png)
 
 **Results Page**
 
-![models](https://github.com/CaryMosley/Mod4ProjectRecommendation/blob/bryan-santos/images/live2.png)
+![models](https://github.com/CaryMosley/Mod4ProjectRecommendation/blob/CaryM/images/live2.png)
 
--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import flask
 import difflib
 import pandas as pd
+import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 import surprise
@@ -14,6 +15,10 @@ del content_matrix['Unnamed: 0']
 content_matrix = content_matrix.to_numpy()
 ratings_df = pd.read_csv('ratings_df.csv')
 del ratings_df['Unnamed: 0']
+
+pop_df = pd.read_csv('pop_df.csv')
+collab_matrix = pd.read_csv('collab_matrix.csv')
+#collab_matrix = collab_matrix.to_numpy()
 
 reader = surprise.Reader(rating_scale=(1, 5))
 data = surprise.Dataset.load_from_df(ratings_df, reader)
@@ -44,6 +49,65 @@ def hybrid_rec(userid, favemovie,n):
     rec_hybrid = rec_hybrid.sort_values('score', ascending=False)
     return rec_hybrid
 
+# This creates the hybrid similarity matrix
+def create_raw(content_matrix,collab_matrix,popularity):
+    '''
+    Creates hybrid matrix for recommendation similarity
+    
+    Parameters:
+        Content_matrix - content-based similarity matrix
+        Collab_matrix - collaborative-filtering similarity matrix
+        Popularity - dataframe with engineered popularity
+        
+    Returns:
+        Aggregated matrix based on popularity
+    '''
+    weights= [0,0]
+    row = 0
+    column = 0
+    if popularity == 4:
+        weights[0]=.15
+    elif popularity ==3:
+        weights[0]=.25
+    elif popularity == 2:
+        weights[0]=.35
+    else:
+        weights[0]=.5
+    weights[1]=1-weights[0]
+    raw_matrix = np.zeros((len(pop_df['Movie']),len(pop_df['Movie'])))
+    size=len(pop_df['Movie'])
+    collab_array = collab_matrix.values
+    for row in range(0,size): 
+        for column in range(size):  
+            raw_matrix[row][column] = collab_array[row][column]*weights[0]+content_matrix[row][column]*weights[1]
+   # Need to set movie titles as row and columns so that we can index and return the recs
+    return raw_matrix
+
+# This creates the hybrid recommendation
+def hybrid_rec2(title,n):
+    '''
+    This function takes in a title and a number of recs 
+    and outputs the number of closests films based on plot 
+    similarity
+    
+    Parameters:
+        Title - movie to base recommendations on
+        n - number of recommendations
+        
+    Returns:
+        Recommended movies based on Hybrid 2 model
+    '''
+    
+    idx = indices[title]
+    popularity = list(pop_df.loc[pop_df['Movie'] == title].Popularity)[0]
+
+    raw_matrix = create_raw(content_matrix,collab_matrix,popularity)
+    sim_scores = list(enumerate(raw_matrix[idx]))
+    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+    sim_scores = sim_scores[1:n+1]
+    movies = [i[0] for i in sim_scores]
+    return titles.iloc[movies].sort_values('rating', ascending=False)
+
 # Set up the main route
 @app.route('/', methods=['GET', 'POST'])
 
@@ -54,52 +118,98 @@ def main():
     if flask.request.method == 'POST':
         m_name = flask.request.form['movie_name']
         m_name = m_name.title()
+        #m_models = flask.request.form['models']
+        m_models = flask.request.form.get('models')
+        m_models = str(m_models)
 #        check = difflib.get_close_matches(m_name,all_titles,cutout=0.50,n=1)
         if m_name not in indices:
             return(flask.render_template('negative.html',name=m_name))
         else:
+            if m_models == "1":
             #print("movie found")
-            result_final = hybrid_rec(343, m_name, 30)
-            #result_final = content_recommendations(m_name, 10)
-            result_final.columns = ['title', 'genres', 'plotsummary', 'rating', 'score']
+                result_final = hybrid_rec(343, m_name, 30)
+                #result_final = content_recommendations(m_name, 10)
+                result_final.columns = ['title', 'genres', 'plotsummary', 'rating', 'score']
 
-            names = []
-            names.append(result_final.iloc[0].title)
-            names.append(result_final.iloc[1].title)
-            names.append(result_final.iloc[2].title)
-            names.append(result_final.iloc[3].title)
-            names.append(result_final.iloc[4].title)
-            names.append(result_final.iloc[5].title)
-            names.append(result_final.iloc[6].title)
-            names.append(result_final.iloc[7].title)
-            names.append(result_final.iloc[8].title)
-            names.append(result_final.iloc[9].title)
+                names = []
+                names.append(result_final.iloc[0].title)
+                names.append(result_final.iloc[1].title)
+                names.append(result_final.iloc[2].title)
+                names.append(result_final.iloc[3].title)
+                names.append(result_final.iloc[4].title)
+                names.append(result_final.iloc[5].title)
+                names.append(result_final.iloc[6].title)
+                names.append(result_final.iloc[7].title)
+                names.append(result_final.iloc[8].title)
+                names.append(result_final.iloc[9].title)
 
-            genres = []
-            genres.append(result_final.iloc[0].genres)
-            genres.append(result_final.iloc[1].genres)
-            genres.append(result_final.iloc[2].genres)
-            genres.append(result_final.iloc[3].genres)
-            genres.append(result_final.iloc[4].genres)
-            genres.append(result_final.iloc[5].genres)
-            genres.append(result_final.iloc[6].genres)
-            genres.append(result_final.iloc[7].genres)
-            genres.append(result_final.iloc[8].genres)
-            genres.append(result_final.iloc[9].genres)
+                genres = []
+                genres.append(result_final.iloc[0].genres)
+                genres.append(result_final.iloc[1].genres)
+                genres.append(result_final.iloc[2].genres)
+                genres.append(result_final.iloc[3].genres)
+                genres.append(result_final.iloc[4].genres)
+                genres.append(result_final.iloc[5].genres)
+                genres.append(result_final.iloc[6].genres)
+                genres.append(result_final.iloc[7].genres)
+                genres.append(result_final.iloc[8].genres)
+                genres.append(result_final.iloc[9].genres)
 
-            plots = []
-            plots.append(result_final.iloc[0].plotsummary)
-            plots.append(result_final.iloc[1].plotsummary)
-            plots.append(result_final.iloc[2].plotsummary)
-            plots.append(result_final.iloc[3].plotsummary)
-            plots.append(result_final.iloc[4].plotsummary)
-            plots.append(result_final.iloc[5].plotsummary)
-            plots.append(result_final.iloc[6].plotsummary)
-            plots.append(result_final.iloc[7].plotsummary)
-            plots.append(result_final.iloc[8].plotsummary)
-            plots.append(result_final.iloc[9].plotsummary)
+                plots = []
+                plots.append(result_final.iloc[0].plotsummary)
+                plots.append(result_final.iloc[1].plotsummary)
+                plots.append(result_final.iloc[2].plotsummary)
+                plots.append(result_final.iloc[3].plotsummary)
+                plots.append(result_final.iloc[4].plotsummary)
+                plots.append(result_final.iloc[5].plotsummary)
+                plots.append(result_final.iloc[6].plotsummary)
+                plots.append(result_final.iloc[7].plotsummary)
+                plots.append(result_final.iloc[8].plotsummary)
+                plots.append(result_final.iloc[9].plotsummary)
 
-            return flask.render_template('positive.html',movie_names=names,movie_genres=genres,movie_plots=plots,search_name=m_name)
+                return flask.render_template('positive.html',movie_names=names,movie_genres=genres,movie_plots=plots,search_name=m_name)
+            else:
+                result_final = hybrid_rec2(m_name, 30)
+                #result_final = content_recommendations(m_name, 10)
+                result_final.columns = ['title', 'genres', 'plotsummary', 'rating']
+
+                names = []
+                names.append(result_final.iloc[0].title)
+                names.append(result_final.iloc[1].title)
+                names.append(result_final.iloc[2].title)
+                names.append(result_final.iloc[3].title)
+                names.append(result_final.iloc[4].title)
+                names.append(result_final.iloc[5].title)
+                names.append(result_final.iloc[6].title)
+                names.append(result_final.iloc[7].title)
+                names.append(result_final.iloc[8].title)
+                names.append(result_final.iloc[9].title)
+
+                genres = []
+                genres.append(result_final.iloc[0].genres)
+                genres.append(result_final.iloc[1].genres)
+                genres.append(result_final.iloc[2].genres)
+                genres.append(result_final.iloc[3].genres)
+                genres.append(result_final.iloc[4].genres)
+                genres.append(result_final.iloc[5].genres)
+                genres.append(result_final.iloc[6].genres)
+                genres.append(result_final.iloc[7].genres)
+                genres.append(result_final.iloc[8].genres)
+                genres.append(result_final.iloc[9].genres)
+
+                plots = []
+                plots.append(result_final.iloc[0].plotsummary)
+                plots.append(result_final.iloc[1].plotsummary)
+                plots.append(result_final.iloc[2].plotsummary)
+                plots.append(result_final.iloc[3].plotsummary)
+                plots.append(result_final.iloc[4].plotsummary)
+                plots.append(result_final.iloc[5].plotsummary)
+                plots.append(result_final.iloc[6].plotsummary)
+                plots.append(result_final.iloc[7].plotsummary)
+                plots.append(result_final.iloc[8].plotsummary)
+                plots.append(result_final.iloc[9].plotsummary)
+
+                return flask.render_template('positive.html',movie_names=names,movie_genres=genres,movie_plots=plots,search_name=m_name)
 
 if __name__ == '__main__':
     app.run(debug=True)
